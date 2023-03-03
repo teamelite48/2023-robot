b710f32f5f0deac54d7eb1f7b6103f70fa934c2c
Matthew Jones <mtjones330@fastmail.com>
2023-03-02 23:34:22 -0500

    Tuned the elbow and stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -112,17 +112,17 @@ public class RobotContainer {
 
   private void initTestController() {
 
-    testController.up.whileTrue(new InstantCommand(() -> armSubsystem.setShoulderMotorSpeed(ArmConfig.SHOULDER_MAX_SPEED)))
+    testController.up.onTrue(new InstantCommand(() -> armSubsystem.setShoulderMotorSpeed(ArmConfig.SHOULDER_MAX_SPEED)))
       .onFalse(new InstantCommand(() -> armSubsystem.setShoulderMotorSpeed(0)));
 
-    testController.down.whileTrue(new InstantCommand(() -> armSubsystem.setShoulderMotorSpeed(-ArmConfig.SHOULDER_MAX_SPEED)))
+    testController.down.onTrue(new InstantCommand(() -> armSubsystem.setShoulderMotorSpeed(-ArmConfig.SHOULDER_MAX_SPEED)))
       .onFalse(new InstantCommand(() -> armSubsystem.setShoulderMotorSpeed(0)));
 
-    // testController.l1.whileTrue(new InstantCommand(() -> armSubsystem.setElbowMotorSpeed(ArmConfig.ELBOW_TEST_SPEED)))
-    //   .onFalse(new InstantCommand(() -> armSubsystem.setElbowMotorSpeed(0)));
+    testController.l1.onTrue(new InstantCommand(() -> armSubsystem.setElbowMotorSpeed(-ArmConfig.ELBOW_MAX_SPEED)))
+      .onFalse(new InstantCommand(() -> armSubsystem.setElbowMotorSpeed(0)));
 
-    // testController.r1.whileTrue(new InstantCommand(() -> armSubsystem.setElbowMotorSpeed(-ArmConfig.WRIST_TEST_SPEED)))
-    //   .onFalse(new InstantCommand(() -> armSubsystem.setElbowMotorSpeed(0)));
+    testController.r1.onTrue(new InstantCommand(() -> armSubsystem.setElbowMotorSpeed(ArmConfig.ELBOW_MAX_SPEED)))
+      .onFalse(new InstantCommand(() -> armSubsystem.setElbowMotorSpeed(0)));
 
     // testController.l2.whileTrue(new InstantCommand(() -> armSubsystem.setWristMotorSpeed(ArmConfig.WRIST_TEST_SPEED)))
     //   .onFalse(new InstantCommand(() -> armSubsystem.setWristMotorSpeed(0)));
--- src/main/java/frc/robot/subsystems/arm/ArmConfig.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConfig.java
@@ -12,7 +12,7 @@ public class ArmConfig {
     public static final double SHOULDER_METERS_FROM_GROUND =  0.35034722;
     public static final double MAX_METERS_PER_SECOND = 0.0762;
 
-    public static final IdleMode MOTOR_IDLE_MODE = IdleMode.kCoast;
+    public static final IdleMode MOTOR_IDLE_MODE = IdleMode.kBrake;
     public static final int ABSOULTE_ENCODER_TICKS_PER_ROTATION = 8192;
     public static final int RELATIVE_ENCODER_TICKS_PER_ROTATION = 42;
 
@@ -22,7 +22,7 @@ public class ArmConfig {
     public static final int SHOULDER_MOTOR_CURRENT_LIMIT = 30;
     public static final boolean SHOULDER_MOTOR_INVERTED = false;
     public static final double SHOULDER_ABSOULTE_ENCODER_POSITION_CONVERSION_FACTOR = 360.0 / (52.0 / 15.0);
-    public static final double SHOULDER_ABSOLUTE_ENCODER_OFFSET = 0.0; // 74.0
+    public static final double SHOULDER_ABSOLUTE_ENCODER_OFFSET = 72.346;
     public static final boolean SHOULDER_ABSOULTE_ENCODER_INVERTED = false;
     public static final double SHOULDER_RELATIVE_ENCODER_POSITION_CONVERSION_FACTOR = 360 / (485.33 / 1.0);
     public static final boolean SHOULDER_ANGLE_STARTS_NEGATIVE = false;
@@ -35,15 +35,15 @@ public class ArmConfig {
 
     public static final int ELBOW_MOTOR_ID = 12;
     public static final int ELBOW_MOTOR_CURRENT_LIMIT = 30;
-    public static final boolean ELBOW_MOTOR_INVERTED = true;
-    public static final double ELBOW_ABSOULTE_ENCODER_POSITION_CONVERSION_FACTOR = 360.0 / (40.0 / 20.0);
-    public static final double ELBOW_ABSOULTE_ENCODER_OFFSET = 0.0;
-    public static final boolean ELBOW_ABSOULTE_ENCODER_INVERTED = false;
+    public static final boolean ELBOW_MOTOR_INVERTED = false;
+    public static final double ELBOW_ABSOULTE_ENCODER_POSITION_CONVERSION_FACTOR = 360.0;
+    public static final double ELBOW_ABSOULTE_ENCODER_OFFSET = 223.0;
+    public static final boolean ELBOW_ABSOULTE_ENCODER_INVERTED = true;
     public static final double ELBOW_RELATIVE_ENCODER_POSITION_CONVERSION_FACTOR = 360.0 / (219.89 / 1.0); // 42.0 / 5.0 ???
     public static final boolean ELBOW_ANGLE_STARTS_NEGATIVE = true;
-    public static final float ELBOW_FORWARD_LIMIT = 90.0f;
-    public static final float ELBOW_REVERSE_LIMIT = -90.0f;
-    public static final double ELBOW_MAX_SPEED = 0.03;
+    public static final float ELBOW_FORWARD_LIMIT = -40.0f;
+    public static final float ELBOW_REVERSE_LIMIT = -320.0f;
+    public static final double ELBOW_MAX_SPEED = 0.2;
     public static final PIDParameters ELBOW_PID = new PIDParameters(0.1, 0.0, 0.0, -ELBOW_MAX_SPEED, ELBOW_MAX_SPEED);
     public static final double ELBOW_SIMULATION_START_ANGLE = -165.0;
 
@@ -53,7 +53,7 @@ public class ArmConfig {
     public static final double WRIST_ABSOLUTE_ENCODER_POSITION_CONVERSION_FACTOR = 2.0 / 1.0;
     public static final double WRIST_ABSOLUTE_ENCODER_OFFSET = 0.0;
     public static final boolean WRIST_ABSOULTE_ENCODER_INVERTED = false;
-    public static final double WRIST_RELATIVE_ENCODER_POSITION_CONVERSION_FACTOR = 10.0 / 1.0; // will increase 30.0 / 1.0
+    public static final double WRIST_RELATIVE_ENCODER_POSITION_CONVERSION_FACTOR = 30.0 / 1.0;
     public static final boolean WRIST_ANGLE_STARTS_NEGATIVE = false;
     public static final float WRIST_FORWARD_LIMIT = 90.0f;
     public static final float WRIST_REVERSE_LIMIT = -90.0f;
