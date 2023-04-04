26e92e4ac58d40f5fa5b0d2c6c32411c29a279c0
Trevor <trev111490@gmail.com>
2023-04-03 20:08:11 -0400

    fixed an auto oopsie

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -176,7 +176,7 @@ public class RobotContainer {
     autoChooser.setDefaultOption("GLHF (Do Nothing)", new WaitCommand(3));
     autoChooser.addOption("Barrier Score Cone Hold Cube Balance", driveSubsystem.getPathPlannerGroupCommand(PathFollowing.BarrierScoreConeHoldCubeBalance));
     autoChooser.addOption("Barrier Score Cone Score Cube", driveSubsystem.getPathPlannerGroupCommand(PathFollowing.BarrierScoreConeScoreCube));
-    //autoChooser.addOption("Barrier Score Cone Score Cube Balance", driveSubsystem.getPathPlannerGroupCommand(PathFollowing.BarrierScoreConeScoreCubeBalance));
+    autoChooser.addOption("Barrier Score Cone Score Cube Balance", driveSubsystem.getPathPlannerGroupCommand(PathFollowing.BarrierScoreConeScoreCubeBalance));
     autoChooser.addOption("Barrier Low 3 Piece", driveSubsystem.getPathPlannerGroupCommand(PathFollowing.BarrierLowThreePiece));
 
     autoChooser.addOption("Wall Score Cone Hold Cube Balance", driveSubsystem.getPathPlannerGroupCommand(PathFollowing.WallScoreConeHoldCubeBalance));
